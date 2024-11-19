a03614d0f87da3b24b931fbfb3412837053af4cd
anonym <anonym@riseup.net>
2024-11-19 12:08:42 +0100

    shfmt

--- config/chroot_local-includes/etc/NetworkManager/dispatcher.d/20-time.sh
+++ config/chroot_local-includes/etc/NetworkManager/dispatcher.d/20-time.sh
@@ -10,12 +10,12 @@ set -u
 
 # Run only when the interface is not "lo":
 if [ -z "$1" ] || [ "$1" = "lo" ]; then
-	exit 0
+    exit 0
 fi
 
 # Run whenever an interface gets "up", not otherwise:
 if [ "$2" != "up" ]; then
-	exit 0
+    exit 0
 fi
 
 /usr/local/lib/run-with-user-env systemctl --no-block --user restart tails-htpdate-notify-user.service
