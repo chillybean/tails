0cde732b8f106bfdcb8a2dea6081279c1a39bec8
boyska <boyska@riseup.net>
2025-01-21 17:53:06 +0100

    explicitly select the intended desktop file
    refs tails#20745

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tps_frontend/views/features_view.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tps_frontend/views/features_view.py
@@ -185,7 +185,7 @@ class FeaturesView(View):
         launch_context = display.get_app_launch_context()  # type: Gdk.AppLaunchContext
         launch_context.set_timestamp(Gtk.get_current_event_time())
         # noinspection PyArgumentList
-        app = Gio.DesktopAppInfo.new("org.boum.tails.AdditionalSoftware.desktop")
+        app = Gio.DesktopAppInfo.new_from_filename("/usr/share/applications/org.boum.tails.AdditionalSoftware.desktop")
         app.launch(context=launch_context)
 
     def on_activate_link(self, label: Gtk.Label, uri: str):
