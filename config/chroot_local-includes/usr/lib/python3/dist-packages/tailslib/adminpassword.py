4dd3428ace80f813938c86baa9c236ac1d3ec009
intrigeri <intrigeri@riseup.net>
2025-01-28 12:36:55 +0000

    Use absolute path (Ruff S607)

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tailslib/adminpassword.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tailslib/adminpassword.py
@@ -2,5 +2,5 @@ import subprocess
 
 
 def is_password_set():
-    output = subprocess.check_output(["passwd", "--status"])
+    output = subprocess.check_output(["/bin/passwd", "--status"])
     return output.split()[1] == b"P"
