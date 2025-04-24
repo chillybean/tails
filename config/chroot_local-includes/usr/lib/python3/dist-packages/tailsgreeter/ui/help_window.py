507f713f7eaffd19b88c2accad9beacb4dc11a53
intrigeri <intrigeri@torproject.org>
2025-04-24 14:35:48 +0000

    Welcome Screen: update required WebKit2 version

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tailsgreeter/ui/help_window.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tailsgreeter/ui/help_window.py
@@ -6,7 +6,7 @@ from tailsgreeter.translatable_window import TranslatableWindow
 from tailsgreeter.ui import _
 
 gi.require_version("Gtk", "3.0")
-gi.require_version("WebKit2", "4.0")
+gi.require_version("WebKit2", "4.1")
 from gi.repository import Gtk, WebKit2
 
 PREFERRED_WIDTH = 800
