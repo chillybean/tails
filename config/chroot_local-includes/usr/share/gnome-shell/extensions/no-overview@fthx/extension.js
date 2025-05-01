80782c6ffada8ce02f785ff83fbb124252279415
intrigeri <intrigeri@torproject.org>
2025-05-01 13:29:58 +0000

    Update no-overview@fthx GNOME Shell extension to v48 for Trixie
    Copied from the upstream v48 tag (commit
    36f1f8a88fe09e4a7c23ba24d06293482f05e215). Their Git log suggests that the
    incompatibility with the window-list extension has been fixed.

--- config/chroot_local-includes/usr/share/gnome-shell/extensions/no-overview@fthx/extension.js
+++ config/chroot_local-includes/usr/share/gnome-shell/extensions/no-overview@fthx/extension.js
@@ -1,56 +1,19 @@
 /*
   No overview at start-up
-  Contributors: @fthx, @fmuellner
+  GNOME Shell 45+ extension
+  Contributors: @fthx
   License: GPL v3
 */
 
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
-const Main = imports.ui.main;
-
-
-class Extension {
-    constructor() {
-        this._realHasOverview = Main.sessionMode.hasOverview;
-    }
-
+export default class NoOverviewExtension {
     enable() {
-        if (!Main.layoutManager._startingUp) {
-            return;
-        }
-
-        /*
-          Since we are based on Debian Bookworm we run GNOME 43 and
-          the appropriate version of this extension is version 13,
-          which simply sets `hasOverview = false` straight in
-          enable(), i.e. the Activities Overview is disabled early in
-          GNOME's startup, when extensions are enabled. This
-          interferes with the window-list extension, making it
-          impossible to exit from the Activities Overview. We work
-          around this by instead delaying that to when GNOME's
-          startup-prepared signal is emitted.
-
-          If you look at the code in gnome-shell-43.9/js/ui/layout.js
-          you can see that after `this.emit('startup-prepared')` it
-          calls `_startupAnimation()`, and in all the three paths it
-          can take we quickly end up in `_startupAnimationComplete()`
-          which emits the startup-complete signal. So very little
-          happens in-between, which probably is why this workaround
-          seems to work fine.
-         */
-        Main.layoutManager.connect('startup-prepared', () => {
-            Main.sessionMode.hasOverview = false;
-        });
-
-        Main.layoutManager.connect('startup-complete', () => {
-            Main.sessionMode.hasOverview = this._realHasOverview;
-        });
+        if (Main.layoutManager._startingUp)
+            Main.layoutManager.connectObject('startup-complete', () => Main.overview.hide(), this);
     }
 
     disable() {
-        Main.sessionMode.hasOverview = this._realHasOverview;
+        Main.layoutManager.disconnectObject(this);
     }
-}
-
-function init() {
-    return new Extension();
 }
