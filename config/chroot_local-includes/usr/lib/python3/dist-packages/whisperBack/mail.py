b353603e62b1239e4f5cf2e477cb1eab595bf020
boyska <boyska@riseup.net>
2025-05-06 15:11:08 +0200

    No need to special-case IPv6 anymore
    First, commit 4081b79 is now in Tails, so the exception message we get
    is good enough.
    
    Second, we really don't expect to be connecting to an IPv6 address,
    because we're not connecting to an onion anymore

--- config/chroot_local-includes/usr/lib/python3/dist-packages/whisperBack/mail.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/whisperBack/mail.py
@@ -67,19 +67,7 @@ def send_message(
     socks.set_default_proxy(socks.SOCKS5, socks_host, socks_port)
     socket.socket = socks.socksocket
 
-    try:
-        # We set a long timeout because Tor is slow
-        smtp = smtplib.SMTP(timeout=120, host=host, port=port)
-    except ValueError as exc:
-        # socks assumes the host resolves to AF_INET and triggers a ValueError
-        # if it's not the case. If a .onion address is given, it resolves to an
-        # AF_INET address and to an AF_INET6 address. If the 1st doesn't connect,
-        # the 2nd is tried, which make socks trigger the ValueError.
-        # This issue is fixed upstream in socks, in which this situation raises
-        # a socket.error (https://github.com/Anorov/PySocks/commit/4081b79)
-        # XXX: this workaround should be removed when a version of socks containing
-        # this commit reaches Tails.
-        raise OSError("PySocks doesn't support IPv6") from exc
-
-    smtp.sendmail(from_address, [to_address], message)
-    smtp.quit()
+    # We set a long timeout because Tor is slow
+    with smtplib.SMTP(host=host, port=port, timeout=120) as smtp:
+        smtp.sendmail(from_address, [to_address], message)
+        smtp.quit()
