851fa49bb8dae3dce78477d5e4e2a250a6ebdd92
boyska <boyska@riseup.net>
2025-05-08 15:19:55 +0200

    Port date extension to class
    see https://gjs.guide/extensions/upgrading/gnome-shell-45.html#extension

--- config/chroot_local-includes/usr/share/gnome-shell/extensions/date@tails.boum.org/extension.js
+++ config/chroot_local-includes/usr/share/gnome-shell/extensions/date@tails.boum.org/extension.js
@@ -5,57 +5,62 @@ import * as ByteArray from 'resource:///org/gnome/shell/byteArray.js';
 
 var settings;
 
-function init() {
-}
+export default class DateExtension {
 
-function overrider(lbl) {
-    var now = new Date();
-    let [res, out] = GLib.spawn_sync(null, ['sudo', '-n', '/usr/local/lib/tails-get-date'], null, GLib.SpawnFlags.SEARCH_PATH, null);
-    if(out == null) {
-        var desired = now.toLocaleString('en-US') + ' GMT';
-    } else {
-        desired = ByteArray.toString(out).trim();
+    function init() {
     }
 
-    var t = lbl.get_text();
-    if (t != desired) {
-        last = t;
-        lbl.set_text(desired);
-    }
-}
-
-var lbl, signalHandlerID, last = "";
-
-function enable() {
-    var sA = Main.panel.statusArea;
-    if (!sA) { sA = Main.panel._statusArea; }
-
-    if (!sA || !sA.dateMenu) {
-        print("Looks like Shell has changed where things live again; aborting.");
-        return;
-    }
-
-    sA.dateMenu.first_child.get_children().forEach(function(w) {
-        // assume that the text label is the first StLabel we find.
-        // This is dodgy behaviour but there's no reliable way to
-        // work out which it is.
-        w.set_style("text-align: center;");
-        if (w.get_text && !lbl) {
-            lbl = w;
+    function overrider(lbl) {
+        var now = new Date();
+        let [res, out] = GLib.spawn_sync(null, ['sudo', '-n', '/usr/local/lib/tails-get-date'], null, GLib.SpawnFlags.SEARCH_PATH, null);
+        if(out == null) {
+            var desired = now.toLocaleString('en-US') + ' GMT';
+        } else {
+            desired = ByteArray.toString(out).trim();
         }
-    });
-    if (!lbl) {
-        print("Looks like Shell has changed where things live again; aborting.");
-        return;
-    }
-    signalHandlerID = lbl.connect("notify::text", overrider);
-    last = lbl.get_text();
-    overrider(lbl);
-}
 
-function disable() {
-    if (lbl && signalHandlerID) {
-        lbl.disconnect(signalHandlerID);
-        lbl.set_text(last);
+        var t = this.lbl.get_text();
+        if (t != desired) {
+            last = t;
+            this.lbl.set_text(desired);
+        }
+    }
+
+
+    enable() {
+        this.lbl = null;
+        signalHandlerID = null;
+        last = "";
+        var sA = Main.panel.statusArea;
+        if (!sA) { sA = Main.panel._statusArea; }
+
+        if (!sA || !sA.dateMenu) {
+            print("Looks like Shell has changed where things live again; aborting.");
+            return;
+        }
+
+        sA.dateMenu.first_child.get_children().forEach(function(w) {
+            // assume that the text label is the first StLabel we find.
+            // This is dodgy behaviour but there's no reliable way to
+            // work out which it is.
+            w.set_style("text-align: center;");
+            if (w.get_text && !this.lbl) {
+                this.lbl = w;
+            }
+        });
+        if (!this.lbl) {
+            print("Looks like Shell has changed where things live again; aborting.");
+            return;
+        }
+        signalHandlerID = this.lbl.connect("notify::text", overrider);
+        last = this.lbl.get_text();
+        overrider(this.lbl);
+    }
+
+    disable() {
+        if (this.lbl && signalHandlerID) {
+            this.lbl.disconnect(signalHandlerID);
+            this.lbl.set_text(last);
+        }
     }
 }
