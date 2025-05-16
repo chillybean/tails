b6c0e0d8daf237afb2c5cc1fb9cda85ac37e8c89
anonym <anonym@riseup.net>
2025-05-16 13:18:36 +0200

    Test suite: make a smart Dogtail::Node.click()
    The hope was that Ponytail would make the click tree API method always
    work, but sometimes it doesn't (e.g. in TCA) and the actions are
    preferred. So let's attempt to make a smarter method that
    works (almost) everywhere.

--- features/support/helpers/dogtail.rb
+++ features/support/helpers/dogtail.rb
@@ -210,7 +210,7 @@ module Dogtail
     end
 
     def actions
-      get_field('actions')
+      get_field('actions').scan(/['"]([^'"]+)['"]/).flatten
     end
 
     def combovalue
@@ -346,8 +346,16 @@ module Dogtail
       run("#{@var}.#{method_call}")
     end
 
-    def click(*args, **kwargs)
-      call_tree_api_method('click', *args, **kwargs)
+    def click
+      # The tree API doesn't always work, so we first try any of the
+      # actions that a click would trigger.
+      preferred_actions = ['click', 'activate', 'open', 'press', 'select', 'toggle']
+      self.actions.each do |action|
+        if preferred_actions.include?(action)
+          return doActionNamed(action)
+        end
+      end
+      call_tree_api_method('click')
     end
 
     def doActionNamed(action_name)
