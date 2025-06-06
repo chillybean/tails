4f165e607106e54419c48269eb20059828560f31
anonym <anonym@riseup.net>
2025-06-06 15:35:40 +0200

    Test suite: work around weird issue when Nautilus is used as FileChooser

--- features/step_definitions/browser.rb
+++ features/step_definitions/browser.rb
@@ -13,7 +13,17 @@ def desktop_portal_save_as(filename: nil, directory: nil, bookmark: false)
   unless directory.nil?
     # Enter the output directory in its text entry
     @screen.press('ctrl', 'l')
-    dialog.child('', roleName: 'text').text = directory
+    # The keyboard shortcut focuses the text entry want to input
+    # directory into, but there's an annoying issue if we also
+    # inputted a filename in the other text entry above; if we did the
+    # other text entry is still focused for a short time, and it loses
+    # its "File Name" name and thus becomes very similar to the text
+    # entry we now want to interact with, making it difficult to
+    # distinguish the two. We do know that the entry we want is
+    # positioned pretty high up in the dialog, so we distinguish them
+    # like that.
+    try_for(10) { dialog.focused_child.position.last < 50 }
+    dialog.focused_child.text = directory
     @screen.press('enter')
     if bookmark
       # Unfortunately when using Dogtail to click a bookmark in the
