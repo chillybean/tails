c24cfa4f9a4d5fcafa27332859482ed2fa3c74f0
anonym <anonym@riseup.net>
2025-06-13 12:47:53 +0200

    Test suite: drop Dogtail workaround

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -496,8 +496,7 @@ Given /^I set the language to (.*) \((.*)\)$/ do |lang, lang_code|
   # so Dogtail is unable to click it directly. We let it grab focus
   # and activate it via the keyboard instead.
   try_for(30) do
-    greeter.child(description: 'Configure Language').grabFocus
-    @screen.press('Return')
+    greeter.child(description: 'Configure Language').click
     # Give Gtk some time to open the language popover
     sleep(1)
     # Check if the language popover is open
