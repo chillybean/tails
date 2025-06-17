19c19006c8c06b927df1cc9f5321fda83165224b
intrigeri <intrigeri@torproject.org>
2025-06-17 15:00:29 +0000

    Fix typos and remove them from the codespell ignore-words-list

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tailsgreeter/ui/help_window.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tailsgreeter/ui/help_window.py
@@ -65,7 +65,7 @@ class GreeterHelpWindow(Gtk.Window, TranslatableWindow):
         )
         self.present()
 
-    def cb_load_started(self, web_view, ressource, request):
+    def cb_load_started(self, web_view, resource, request):
         if not request.get_uri().startswith("file://"):
             webbrowser.open_new(request.get_uri())
             request.set_uri(web_view.get_uri())
