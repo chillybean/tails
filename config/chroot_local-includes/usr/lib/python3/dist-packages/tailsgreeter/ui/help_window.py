87e93a1143b1327c2dd6c9ba0cb4e9bce262d7d3
intrigeri <intrigeri@torproject.org>
2025-06-17 15:53:34 +0000

    Lint

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tailsgreeter/ui/help_window.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tailsgreeter/ui/help_window.py
@@ -7,7 +7,7 @@ from tailsgreeter.ui import _
 
 gi.require_version("Gtk", "3.0")
 gi.require_version("WebKit2", "4.0")
-from gi.repository import Gtk, WebKit2
+from gi.repository import Gtk, WebKit2  # noqa: E402
 
 PREFERRED_WIDTH = 800
 
