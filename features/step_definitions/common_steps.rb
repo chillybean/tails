7c2ff33532e8367eb7bd9f8395553d7d053a9d3f
anonym <anonym@riseup.net>
2025-09-09 14:28:51 +0200

    Test suite: add support for pkexec'd applications to launch_app()

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -1055,7 +1055,7 @@ def switch_input_source
   sleep 1
 end
 
-def launch_app(desktop_file_name, app_name, user: LIVE_USER, timeout: 30, check_started: true)
+def launch_app(desktop_file_name, app_name, user: LIVE_USER, timeout: 30, check_started: true, pkexec_root: false)
   # We use systemd-run to launch the app, because we want the app to run
   # in the active systemd login session, so that polkit rules for active
   # sessions apply to it.
@@ -1064,11 +1064,13 @@ def launch_app(desktop_file_name, app_name, user: LIVE_USER, timeout: 30, check_
          '/usr/local/bin/gtk-abspath-launch',
          "/usr/share/applications/#{desktop_file_name}",].join(' ')
   $vm.execute(cmd, user:)
+  deal_with_polkit_prompt(@sudo_password) if pkexec_root
   return unless check_started
 
   app = nil
+  dogtail_user = pkexec_root ? 'root' : user
   try_for(timeout) do
-    app = Dogtail::Application.new(app_name)
+    app = Dogtail::Application.new(app_name, user: dogtail_user)
   end
   app
 end
