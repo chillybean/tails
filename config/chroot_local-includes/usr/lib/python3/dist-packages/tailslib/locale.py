f89e1042276bb024e2b6c56f2156554eb1157edc
anonym <anonym@riseup.net>
2025-09-10 12:44:30 +0200

    Migrate from deprecated /etc/default/locale to /etc/locale.conf

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tailslib/locale.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tailslib/locale.py
@@ -2,7 +2,7 @@ import os
 
 
 def apply_selected_locale():
-    with open("/etc/default/locale") as f:
+    with open("/etc/locale.conf") as f:
         for line in f:
             k, v = line.strip().split("=", 1)
             os.environ[k] = v
