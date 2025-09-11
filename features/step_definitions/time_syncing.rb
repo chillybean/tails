8762690cfa4177be1b53e34545167b37ab5caf6e
intrigeri <intrigeri@torproject.org>
2025-09-11 12:40:23 +0000

    Test suite: fix checking if systemd advanced the clock
    The "journalctl | grep 'System time before build time, advancing clock'" command
    always succeeded, somewhat hilariously… by finding *itself* in the Journal:
    
      Remote shell: sh_call returned: [0, "Jun 26 14:58:42 amnesia systemd[1]: System time before build time, advancing clock.\nAug 21 08:50:00 amnesia tails-autotest-remote-shell[3108]: INFO:root:Executing as root: journalctl | grep 'System time before build time, advancing clock'\n", ""]
    
    I'm recycling an unused method that, if used, would likely introduce other
    instances of the exact same bug that I just fixed in the previous commit. Let's
    hope the availability of this method will avoid doing the same mistake again.

--- features/step_definitions/time_syncing.rb
+++ features/step_definitions/time_syncing.rb
@@ -129,10 +129,12 @@ Then /^the system clock is just past Tails' source date$/ do
 
   if diff > max_diff
     # In this case the only acceptable explanation is that systemd
-    # adjusted the time.
-    systemd_has_adjusted_time = $vm.execute(
-      "journalctl | grep 'System time before build time, advancing clock'"
-    ).success?
+    # adjusted the time, because its build time is more recent
+    # than $SOURCE_DATE_EPOCH.
+    systemd_has_adjusted_time = systemd_journal_includes(
+      'System time before build time, advancing clock.',
+      journalctl_args: ['_PID=1'],
+    )
     unless systemd_has_adjusted_time
       raise(
         "The system time (#{system_time}) is more than #{max_diff} seconds " \
