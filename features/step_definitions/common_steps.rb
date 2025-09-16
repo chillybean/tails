9b18627f72abe0b2ad0779de3caf091b0f9c0a0b
intrigeri <intrigeri@torproject.org>
2025-09-16 08:55:56 +0000

    Test suite: make journalctl options vs. matches ordering more future-proof
    The documented syntax is:
    
      journalctl [OPTIONS...] [MATCHES...]
    
    Currently passing an option after a match works, but I could imagine the
    developers later changing the code and only supporting what had been documented
    all along.

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -1781,12 +1781,16 @@ end
 # In many cases this is superior to a naive "journalctl | grep", which
 # will find itself because tails-autotest-remote-shell logs the
 # commands its executes.
-def systemd_journal_includes(message, journalctl_args: [], regexp: false)
-  matcher = regexp ? '--grep' : 'MESSAGE'
+def systemd_journal_includes(message, options: [], matches: [], regexp: false)
+  if regexp
+    options.append("--grep='#{message}'")
+  else
+    matches.append("MESSAGE='#{message}'")
+  end
   $vm.execute(
     'journalctl --boot --output=cat ' \
-    "#{journalctl_args.join(' ')} " \
-    "#{matcher}='#{message}' "\
+    "#{options.join(' ')} " \
+    "#{matches.join(' ')} " \
     '| wc -l'
   ).stdout.to_i.positive?
 end
