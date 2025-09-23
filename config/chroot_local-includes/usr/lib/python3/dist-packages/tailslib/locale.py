c07909e89e5c41f76923a875f0a3d0a1a31df451
anonym <anonym@riseup.net>
2025-09-23 15:13:17 +0200

    Parse /etc/locale.conf accurately
    boyska spotted that in locale.conf(5) that its format is the same as
    described in os-release(5), which proposes a python implementation for
    partsin in the "Example 5. Reading os-release in python(1) (any
    version)" section. We base our implementation on that but fix several
    issues:
    
    * Most importantly, quoting for values is now optional. Actually we
      over-accept now by not requiring quoting for values that "include
      spaces, semicolons or other special characters outside of A–Z, a–z,
      0–9" (see os-release(5)). Note that the more modern solution from
      Example 4, platform.freedesktop_os_release(), also does that.
    * Log bad lines consistently
    * Allow single character key names
    * Allow lower-case in key names
    * Allow key names starting with _

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tailslib/locale.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tailslib/locale.py
@@ -1,8 +1,40 @@
+import ast
 import os
+import re
+import sys
+import typing
 
 
+# According to locale.conf(5) it uses the same format as described in
+# os-release(5), so we base the code below on its "Example 5. Reading
+# os-release in python(1) (any version)" but fix several issues with
+# it (see discussion on tails!2459).
+def read_locale_conf() -> typing.Generator[tuple[str, str]]:
+    filename = "/etc/locale.conf"
+    f = open(filename)
+    for line_number, line in enumerate(f, start=1):
+        line = line.rstrip()  # noqa: PLW2901
+        if not line or line.startswith("#"):
+            continue
+        name = None
+        val = None
+        m = re.match(r"([a-zA-Z_][a-zA-Z_0-9]*)=(.*)", line)
+        if m:
+            name, val = m.groups()
+            if val and val[0] in ['"', "'"]:
+                try:
+                    val = ast.literal_eval(val)
+                except ValueError:
+                    val = None
+        if name is not None and val is not None:
+            yield name, val
+        else:
+            print(f"{filename}:{line_number}: bad line {line!r}", file=sys.stderr)
+
+
+# Sets environment variables according to /etc/locale.conf
 def apply_selected_locale():
-    with open("/etc/locale.conf") as f:
-        for line in f:
-            k, v = line.strip().split("=", 1)
-            os.environ[k] = v
+    try:
+        os.environ.update(read_locale_conf())
+    except FileNotFoundError:
+        pass
