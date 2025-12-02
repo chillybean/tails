e5097be262ccee19ec65d7b1641018faa4e5f60f
anonym <anonym@riseup.net>
2025-12-02 12:52:09 +0100

    Test suite: make tails#20282 easier to identify

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -439,7 +439,13 @@ Given /^the computer (?:re)?boots Tails$/ do
     # rest of the session. That window is closed once the Welcome
     # Screen appears, so we wait for that to happen using image
     # matching.
-    @screen.wait('TailsGreeter.png', 60)
+    found = @screen.wait_any(
+      ['TailsGreeter.png', 'PlymouthGraphicsCardFailureMessage.png'], 60
+    )
+    if found == 'PlymouthGraphicsCardFailureMessage.png'
+      raise 'Known issue #20282: Error starting GDM with your graphics card'
+    end
+
     # Enable GNOME introspection for Dogtail and Ponytail
     $vm.execute_successfully('gnome-extensions enable automated-testing@tails.net',
                              user: 'Debian-gdm')
