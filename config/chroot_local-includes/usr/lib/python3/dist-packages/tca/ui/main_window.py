d5f96420305cb6bb18cefb3e516ba3d560b134df
boyska <boyska@riseup.net>
2025-12-17 09:09:35 +0100

    Use a better definition for "Glade files" now that it isn't in Debian anymore
    We're moving away from Glade, and glade is not the only implementation
    that supports GtkBuilder UI definitions
    
    refs #21282

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tca/ui/main_window.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tca/ui/main_window.py
@@ -1141,7 +1141,7 @@ class TCAMainWindow(
 
     def get_object(self, name: str):
         """
-        Get an object from glade file.
+        Get an object from the Gtk.Builder UI definition.
 
         This is a shortcut over self.builder.get_object which takes steps into account
         """
