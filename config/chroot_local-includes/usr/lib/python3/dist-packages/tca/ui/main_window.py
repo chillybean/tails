d5e035c83fcfc2064f01df033d62802eba8cf183
boyska <boyska@riseup.net>
2026-01-08 16:36:18 +0100

    update comment still mentioning glade

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tca/ui/main_window.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tca/ui/main_window.py
@@ -1281,7 +1281,7 @@ class ConnectionProgress:
     """
     This class "handles" the progress bar in the final screen.
 
-    Probably the right approach would have been to subclass Gtk.ProgressBar, but subclassing and glade are
+    Probably the right approach would have been to subclass Gtk.ProgressBar, but subclassing and GtkBuilder are
     hard to combine.
     """
 
