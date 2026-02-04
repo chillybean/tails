d45b40becd354060681b7638a62e498d25660356
intrigeri <intrigeri@torproject.org>
2026-02-04 15:37:27 +0000

    tailslib.migration: allow using on the command line as amnesia
    I need to interact with this subsystem from a shell script, and I'd rather not
    re-implement it all in shell, so let's make the bits I need reusable.
    
    Also, relax the permissions a bit so we're allowed to tell whether the migration
    succeeded when we're running from said shell script, which is running as amnesia.

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tailslib/migration.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tailslib/migration.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 
 
@@ -9,10 +10,9 @@ MIGRATIONS_DIR = (
 
 class Migration:
     def __init__(self, Id: str):
-        state_dir = MIGRATIONS_DIR / Id
-        state_dir.mkdir(mode=0o700, exist_ok=True)
-        self.success_file = state_dir / "success"
-        self.failure_file = state_dir / "failure"
+        self.state_dir = MIGRATIONS_DIR / Id
+        self.success_file = self.state_dir / "success"
+        self.failure_file = self.state_dir / "failure"
 
     @property
     def succeeded(self):
@@ -20,7 +20,19 @@ class Migration:
 
     @succeeded.setter
     def succeeded(self, result: bool) -> None:
+        self.state_dir.mkdir(mode=0o750, exist_ok=True)
+        shutil.chown(self.state_dir, group="amnesia")
         if result:
             self.success_file.touch()
         else:
             self.failure_file.touch()
+
+
+if __name__ == "__main__":
+    import sys
+
+    migration = Migration(sys.argv[1])
+    if migration.succeeded:
+        sys.exit(0)
+    else:
+        sys.exit(1)
