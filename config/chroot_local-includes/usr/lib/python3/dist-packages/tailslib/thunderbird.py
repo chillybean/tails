39da700dc89dd134e4cc4343e73c5ff7ac85020e
intrigeri <intrigeri@torproject.org>
2026-02-09 16:17:35 +0000

    Refactoring: move code to function

--- config/chroot_local-includes/usr/lib/python3/dist-packages/tailslib/thunderbird.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/tailslib/thunderbird.py
@@ -17,3 +17,26 @@ def thunderbird_l10n_package() -> str | None:
             return package
 
     return None
+
+
+def thunderbird_packages() -> set[str]:
+    packages = {
+        "thunderbird",
+        # This list has to be kept in sync' with our list of
+        # tier-1 languages:
+        # https://tails.net/contribute/how/translate/#tier-1-languages
+        "thunderbird-l10n-ar",
+        "thunderbird-l10n-de",
+        "thunderbird-l10n-es-es",
+        "thunderbird-l10n-fr",
+        "thunderbird-l10n-id",
+        "thunderbird-l10n-it",
+        "thunderbird-l10n-pt-br",
+        "thunderbird-l10n-ru",
+        "thunderbird-l10n-tr",
+        "thunderbird-l10n-zh-cn",
+    }
+    l10n_package = thunderbird_l10n_package()
+    if l10n_package is not None:
+        packages |= {l10n_package}
+    return packages
