a7af81f34ae53c7546ba5794f66bb2cb04711765
anonym <anonym@riseup.net>
2026-03-04 10:18:06 +0100

    Test suite: inline otherwise unused step

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -689,7 +689,7 @@ Given /^I successfully configure Tor$/ do
 end
 
 Then /^I wait until Tor is ready$/ do
-  step 'Tor has built a circuit'
+  wait_until_tor_is_working
   step 'the time has synced'
   debug_log('user_wants_pluggable_transports = ' \
            "#{@user_wants_pluggable_transports} " \
@@ -725,10 +725,6 @@ Then /^I wait until Tor is ready$/ do
       raise "The system is not fully running yet:\n#{jobs}\n#{units_status}"
     end
   end
-end
-
-Given /^Tor has built a circuit$/ do
-  wait_until_tor_is_working
 end
 
 class TimeSyncingError < StandardError
